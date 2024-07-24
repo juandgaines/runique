31ad655ebb7355a76663093711811983c2a37ada
Juan Gaines <juan.gaines@platzi.com>
2024-07-24 17:40:07 -0500

    feature: first convention plugin with build typs

--- build-logic/convention/src/main/java/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidApplicationConventionPlugin.kt
@@ -1,4 +1,6 @@
 import com.android.build.api.dsl.ApplicationExtension
+import com.juandgaines.convention.ExtensionType
+import com.juandgaines.convention.configureBuildTypes
 import com.juandgaines.convention.configureKotlinAndroid
 import com.juandgaines.convention.libs
 import org.gradle.api.Plugin
@@ -22,6 +24,7 @@ class AndroidApplicationConventionPlugin:Plugin<Project> {
                     versionName = libs.findVersion("projectVersionName").get().toString()
                 }
                 configureKotlinAndroid(this)
+                configureBuildTypes(this, ExtensionType.APPLICATION)
             }
         }
     }
---
+++ build-logic/convention/src/main/java/com/juandgaines/convention/BuildTypes.kt
@@ -0,0 +1,65 @@
+package com.juandgaines.convention
+
+import com.android.build.api.dsl.CommonExtension
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
+import com.android.build.api.dsl.BuildType
+
+internal fun Project.configureBuildTypes(
+    commonExtension: CommonExtension<*, *, *, *, *>,
+    extensionType: ExtensionType
+){
+    commonExtension.run {
+        buildFeatures {
+            buildConfig = true
+        }
+        val apiKey = gradleLocalProperties(rootDir).getProperty("API_KEY")
+        when (extensionType) {
+            ExtensionType.APPLICATION -> {
+                extensions.configure<com.android.build.api.dsl.ApplicationExtension> {
+
+                    buildTypes {
+                        debug {
+                            configureDebugBuildType(apiKey)
+                        }
+                        release {
+                            configureReleaseBuildType(commonExtension, apiKey)
+                        }
+                    }
+                }
+            }
+            ExtensionType.LIBRARY -> {
+                extensions.configure<com.android.build.api.dsl.LibraryExtension> {
+                    buildTypes {
+                        debug {
+                            configureDebugBuildType(apiKey)
+                        }
+                        release {
+                            configureReleaseBuildType(commonExtension, apiKey)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+private fun BuildType.configureDebugBuildType(apiKey: String) {
+    buildConfigField("String", "API_KEY", "\"$apiKey\"")
+    buildConfigField("String", "BASE_URL", "\"https://runique.pl-coding.com:8080\"")
+}
+
+private fun BuildType.configureReleaseBuildType(
+    commonExtension: CommonExtension<*, *, *, *, *>,
+    apiKey: String
+) {
+    buildConfigField("String", "API_KEY", "\"$apiKey\"")
+    buildConfigField("String", "BASE_URL", "\"https://runique.pl-coding.com:8080\"")
+
+    isMinifyEnabled = true
+    proguardFiles(
+        commonExtension.getDefaultProguardFile("proguard-android-optimize.txt"),
+        "proguard-rules.pro"
+    )
+}
