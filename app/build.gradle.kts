f8630c4de83c5ac338b734f95bc25dec44345856
Juan Gaines <juan.gaines@platzi.com>
2024-07-24 18:18:25 -0500

    feature: Android compose plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     alias(libs.plugins.mapsplatform.secrets.plugin)
-    alias(libs.plugins.runique.android.application)
+    alias(libs.plugins.runique.android.application.compose)
 }
 
 android {
@@ -13,12 +13,6 @@ android {
         }
     }
 
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.10"
-    }
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
