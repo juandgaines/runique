5bed32aeace89cb3f639e08809b371bb90d19f0e
Juan Gaines <juan.gaines@platzi.com>
2024-07-26 08:46:51 -0500

    feature: Android Library plugin

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -24,5 +24,10 @@ gradlePlugin {
             id = "runique.android.application.compose"
             implementationClass = "AndroidApplicationComposeConventionPlugin"
         }
+
+        register("androidLibrary") {
+            id = "runique.android.library"
+            implementationClass = "AndroidLibraryConventionPlugin"
+        }
     }
 }
