9ec900e4b9987f11a3ec5f3265061735f04943c3
Juan Gaines <juan.gaines@platzi.com>
2024-07-26 10:22:55 -0500

    feature:AndroidRoomConventionPlugin

--- build-logic/convention/src/main/java/AndroidLibraryComposeConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidLibraryComposeConventionPlugin.kt
@@ -1,14 +1,8 @@
 import com.android.build.api.dsl.LibraryExtension
-import com.juandgaines.convention.ExtensionType
 import com.juandgaines.convention.configureAndroidCompose
-import com.juandgaines.convention.configureBuildTypes
-import com.juandgaines.convention.configureKotlinAndroid
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.getByType
-import org.gradle.kotlin.dsl.kotlin
 
 class AndroidLibraryComposeConventionPlugin:Plugin<Project>{
 
