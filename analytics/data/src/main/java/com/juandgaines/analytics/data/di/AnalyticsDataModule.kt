0c082fb8b46cd7ccbc828041a2b738168e7b91aa
Juan Gaines <juan.gaines@platzi.com>
2024-08-20 09:46:58 -0500

    feat: analytics repository implementation

--- analytics/data/src/main/java/com/juandgaines/analytics/data/di/AnalyticsDataModule.kt
+++ analytics/data/src/main/java/com/juandgaines/analytics/data/di/AnalyticsDataModule.kt
@@ -3,9 +3,13 @@ package com.juandgaines.analytics.data.di
 import org.koin.core.module.dsl.singleOf
 import com.juandgaines.analytics.data.RoomAnalyticsRepository
 import com.juandgaines.analytics.domain.AnalyticsRepository
+import com.juandgaines.core.database.RunDataBase
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val analyticsModule = module {
     singleOf(::RoomAnalyticsRepository).bind<AnalyticsRepository>()
+    single {
+        get<RunDataBase>().analyticsDao()
+    }
 }
--- core/database/src/main/java/com/juandgaines/core/database/RunDataBase.kt
+++ core/database/src/main/java/com/juandgaines/core/database/RunDataBase.kt
@@ -2,6 +2,7 @@ package com.juandgaines.core.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.juandgaines.core.database.dao.AnalyticsDao
 import com.juandgaines.core.database.dao.RunDao
 import com.juandgaines.core.database.dao.RunPendingSyncDao
 import com.juandgaines.core.database.entity.DeletedRunSyncEntity
@@ -19,4 +20,5 @@ import com.juandgaines.core.database.entity.RunPendingSyncEntity
 abstract class RunDataBase :RoomDatabase() {
     abstract fun runDao(): RunDao
     abstract fun runPendingSyncDao(): RunPendingSyncDao
+    abstract fun analyticsDao(): AnalyticsDao
 }
