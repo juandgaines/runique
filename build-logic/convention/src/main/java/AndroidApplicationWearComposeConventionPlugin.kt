408c348de4d168f4294ff7296e99876bd1af3eb1
Juan Gaines <juan.gaines@platzi.com>
2024-08-22 08:39:08 -0500

    feature: wear os folder structure

---
+++ build-logic/convention/src/main/java/AndroidApplicationWearComposeConventionPlugin.kt
@@ -0,0 +1,23 @@
+import com.juandgaines.convention.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+
+class AndroidApplicationWearComposeConventionPlugin:Plugin<Project> {
+    override fun apply(target: Project) {
+        target.run {
+            pluginManager.run {
+                apply("runique.android.application.compose")
+                apply("org.jetbrains.kotlin.plugin.compose")
+            }
+
+            dependencies {
+                "implementation"(libs.findLibrary("androidx.wear.compose.material").get())
+                "implementation"(libs.findLibrary("androidx.wear.compose.foundation").get())
+                "implementation"(libs.findLibrary("androidx.wear.compose.ui.tooling").get())
+                "implementation"(libs.findLibrary("play.services.wearable").get())
+            }
+
+        }
+    }
+}
---
+++ wear/run/domain/src/main/java/com/juandgaines/wear/run/domain/MyClass.kt
@@ -0,0 +1,4 @@
+package com.juandgaines.wear.run.domain
+
+class MyClass {
+}
