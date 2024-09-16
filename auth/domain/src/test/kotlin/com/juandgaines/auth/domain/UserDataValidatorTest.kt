7c9406e6e86f4243d7d772e2220f96271e787c3b
Juan Gaines <juan.gaines@platzi.com>
2024-09-16 10:33:58 -0500

    featur: setting convention plugin and libraries

---
+++ auth/domain/src/test/kotlin/com/juandgaines/auth/domain/UserDataValidatorTest.kt
@@ -0,0 +1,12 @@
+package com.juandgaines.auth.domain
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import org.junit.jupiter.api.Test
+
+class UserDataValidatorTest{
+    @Test
+    fun testAddingTwoNumbers(){
+        assertThat(2 + 2).isEqualTo(4)
+    }
+}
